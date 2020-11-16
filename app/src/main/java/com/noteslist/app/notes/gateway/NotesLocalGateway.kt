a9c248935e2855e1a9a7503139516359378853b9
Valentyn <cfunsv10@gmail.com>
2020-11-16 12:52:45 +0200

    update code documentatio

--- app/src/main/java/com/noteslist/app/notes/gateway/NotesLocalGateway.kt
+++ app/src/main/java/com/noteslist/app/notes/gateway/NotesLocalGateway.kt
@@ -10,34 +10,30 @@ interface NotesLocalGateway {
 
     /**
      * Used to retrieve list of notes from the local repo
-     * @return RX Flowable with list of notes
+     * @return [Flow] ]with list of notes
      */
     fun getNotes(): Flow<List<Note>>
 
     /**
      * Used to save list of notes to the local repo
      * @param notes - list of note models to be saved
-     * @return RX Completable that signals about operation complete or error
      */
     suspend fun saveNotes(notes: List<Note>)
 
     /**
      * Used to a single note to the local repo
      * @param note - model of Note to be saved
-     * @return RX Completable that signals about operation complete or error
      */
     suspend fun saveNote(note: Note)
 
     /**
      * Used to delete single note from the local repo
      * @param id - of the Note in the local epo
-     * @return RX Completable that signals about operation complete or error
      */
     suspend fun deleteNote(id: String)
 
     /**
      * Used to clear local repo of notes
-     * @return RX Completable that signals about operation complete or error
      */
     suspend fun deleteAll()
 }
--- app/src/main/java/com/noteslist/app/notes/gateway/NotesRemoteGateway.kt
+++ app/src/main/java/com/noteslist/app/notes/gateway/NotesRemoteGateway.kt
@@ -10,14 +10,14 @@ interface NotesRemoteGateway {
 
     /**
      * Used to retrieve list of notes from the remote repo
-     * @return RX Single with list of notes from the response
+     * @return [Flow] with actual list of notes from the remote repo
      */
     fun getNotes(): Flow<List<Note>>
 
     /**
      * Used to add new note to the remote repo
      * @param text of new note
-     * @return RX Single with Note model, that contains added text and generated id in
+     * @return [Note] model, that contains added text and generated id in
      * the remote repo
      */
     suspend fun addNote(text: String): Note
@@ -25,14 +25,14 @@ interface NotesRemoteGateway {
     /**
      * Used to edit existing note in the remote repo
      * @param note - note model to be edited with new fields values
-     * @return RX Single with Note model, that contains edited text
+     * @return [Note] model, that contains edited text
      */
     suspend fun saveNote(note: Note): Note
 
     /**
      * Used to delete existing note in the remote repo
      * @param id - id of the note model to be deleted from the remote repo
-     * @return RX Single with Note model, that contains edited text
+     * @return [Note] model, that contains edited text
      */
     suspend fun deleteNote(id: String)
 }
